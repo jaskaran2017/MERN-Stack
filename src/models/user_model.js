47492f406ebd818d42d6076ac9a421ac3b80996b
jasjit <jaskaranminhas2017@gmail.com>
2021-07-30 23:39:59 +0400

    database added

---
+++ src/models/user_model.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// here we are defining the requires field for user authentication
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      index: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      index: true,
+      lowercase: true,
+    },
+    hash_password: {
+      type: String,
+      trim: true,
+      required: true,
+      min: 8,
+      max: 15,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    contact: {
+      type: String,
+    },
+    profilePic: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// hashing the plain password with bcrypt library
+
+userSchema.virtual("password").set(function (password) {
+  this.hash_password = bcrypt.hashSync(password, 10);
+});
+
+// making comparision of password with stored password for authentication
+
+userSchema.method = {
+  authenticate: function (password) {
+    return bcrypt.compareSync(password, this.hash_password);
+  },
+};
+
+module.exports = mongoose.model("user_model", userSchema);
--- src/server.js
+++ src/server.js
@@ -2,12 +2,33 @@ const express = require("express");
 const env = require("dotenv");
 // const bodyParser = require("body-parser"); this dev dependancy is depricated.
 const app = express();
+const mongoose = require("mongoose");
+
+// routes
+const userRoutes = require("./routes/users");
 
 // calling environment variables or constants here
 env.config();
 
+//mongodb connection string
+//mongodb+srv://root:<password>@mern-stack.jzjaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.Mongo_db_user}:${process.env.Mongo_db_password}@mern-stack.jzjaz.mongodb.net/${process.env.Mongo_db_database}?retryWrites=true&w=majority`,
+
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
+
 //adding a middleware to send data
 app.use(express.json());
+app.use("/api", userRoutes);
 
 app.get("/", (req, res, next) => {
   res.status(200).json({
